0f580c6382d4d5c358299397a90c01f71e35ace0
Murtaza Pakawala <mypaka52@gmail.com>
2022-08-30 00:22:03 +1000

    have changed to a function which validated the login authentication

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
 const puppeteer = require("puppeteer");
 
 async function start() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://webcms3.cse.unsw.edu.au/login");
-  await page.screenshot({ path: "amazing1.png" });
-  await page.type("#zid", "z540571");
-  await page.type("#password", "bond@2003");
-  await page.click(".well button");
-  await page.waitForNavigation();
-  await page.screenshot({ path: "amazing2.png" });
-  console.log(browser.pages());
-  await browser.close();
-}
+  async function login(zid, password) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto("https://webcms3.cse.unsw.edu.au/login");
+    await page.screenshot({ path: "amazing1.png" });
+    await page.type("#zid", `${zid}`);
+    await page.type("#password", `${password}`);
+    await page.click(".well button");
+    await page.waitForNavigation();
+    await page.screenshot({ path: "amazing2.png" });
+    const url = await page.url();
+    await browser.close();
+    if (url === "https://webcms3.cse.unsw.edu.au/login") {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
+  const ans = await login("", "");
+  console.log(ans);
+}
 start();
